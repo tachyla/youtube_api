70228962862016123fa48bc5944a47ed8b36b399
tachyla <tachylad.murray@gmail.com>
2017-04-12 09:59:29 -0700

    event listner to submit button

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-var me = 'Yolo';
+var OMDB_URL = "https://www.omdbapi.com/";
+var query = {
+	s: 'batman',
+	r: 'json'
+};
+
+
+let userInput = $("#searchbar").val();
+
+$(document).ready(function() {
+
+	$("#submit").on('click',function(event) {
+		event.preventDefault();
+		console.log(userInput);
+	});
+	
+})
+
+//$.getJSON(website, searchQ, callback);
+
+
+
+//render function
+
+
+
+
+	
+
+
+
+
